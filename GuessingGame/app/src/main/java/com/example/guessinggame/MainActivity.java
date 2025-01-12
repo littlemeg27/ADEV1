89ebcf9f3296aece5063efb7f860bd2b54caa1f0
Brenna Pavlinchak <littlemeg27@student.fullsail.edu>
2025-01-12 18:46:57 -0500

    Getting rid of warnings

--- GuessingGame/app/src/main/java/com/example/guessinggame/MainActivity.java
+++ GuessingGame/app/src/main/java/com/example/guessinggame/MainActivity.java
@@ -1,22 +1,13 @@
 package com.example.guessinggame;
 
 import android.os.Bundle;
-import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.Random;
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 // Brenna Pavlinchak
 // Term C202501
